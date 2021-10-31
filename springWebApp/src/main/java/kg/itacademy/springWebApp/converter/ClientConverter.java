11e78363fcd1072598e0557199ae8198d961379c
bermetelmirova <bermetk.6789@gmail.com>
2021-10-31 17:35:11 +0600

    edit project

--- springWebApp/src/main/java/kg/itacademy/springWebApp/converter/ClientConverter.java
+++
@@ -1,7 +0,0 @@
-package kg.itacademy.springWebApp.converter;
-
-public class ClientConverter {
-    //public ClientConverter() {
-        //super(Function<ClientModel, Client> fromModel, Function<Client, ClientModel> fromEntity);
-
-}
